a12c1db8d5f5b7dfda09e6b777a1429e0aa33821
isabelarapetti <isabelarapetti@gmail.com>
2021-08-06 23:06:32 -0300

    basic creates nodes

--- Program.cs
+++ Program.cs
@@ -8,15 +8,18 @@ namespace ConsoleAppSimple
         static void Main(string[] args)
         {
             Console.WriteLine("Aloha");
+
+            //initialize filesystem
+            FileSystemElement fs = new FileSystemElement("Root", null);
+
             while (true)
             {
                 var input = System.Console.ReadLine().Trim();
                 var arguments = new List<string>(input.Split(' '));
                 var commandName = arguments[0];
 
-                //var command = SimpleCommandFactory.CreateCommand(commandName);
-
-                //command.Execute(arguments);
+                var command = SimpleCommandFactory.CreateCommand(commandName, fs); //TODO remove fs
+                command.Execute(arguments,fs);
             }
         }
     }
@@ -24,55 +27,34 @@ namespace ConsoleAppSimple
     #region COMMANDS REGION
     public interface ICommand
     {
-        void Execute(List<string> arguments);
+        void Execute(List<string> arguments, FileSystemElement fs);
     }
 
     public class SimpleCommandFactory
     {
-        //create command instance 
+        public static ICommand CreateCommand(String commandName, FileSystemElement fs)
+        {
+            switch (commandName)
+            {
+                case "pwd":
+                    return new PrintDirectoryCommand();
+                case "mkdir":
+                    return new CreateDirectoryCommand();
+                case "ls":
+                    return new ListCommand();
+                case "cd":
+                    return new ChangeDirectoryCommand();
+                case "touch":
+                    return new CreateFileCommand();
+                case "quit":
+                    return new QuitCommand();
+                default:
+                    return new UnrecognizedCommand();
+            }
+        }
     }
 
     //Concrete Commands
-    public class CreateFileCommand : ICommand
-    {
-        public void Execute(List<string> arguments)
-        {
-            Console.WriteLine($"Hello World! CreateFileCommand");
-        }
-    }
-
-    public class PrintDirectoryCommand : ICommand
-    {
-        public void Execute(List<string> arguments)
-        {
-            Console.WriteLine($"Hello World! PrintDirectoryCommand");
-        }
-    }
-
-    public class ListCommand : ICommand
-    {
-        public void Execute(List<string> arguments)
-        {
-            Console.WriteLine($"Hello World! ListCommand");
-        }
-    }
-
-    public class ChangeDirectoryCommand : ICommand
-    {
-        public void Execute(List<string> arguments)
-        {
-            Console.WriteLine($"Hello World! ChangeDirectoryCommand");
-        }
-    }
-
-    public class CreateDirectoryCommand : ICommand
-    {
-        public void Execute(List<string> arguments)
-        {
-            Console.WriteLine($"Hello World! CreateDirectoryCommand");
-        }
-    }
-
     public class QuitCommand : ICommand
     {
         public void Execute(List<string> arguments)
@@ -81,16 +63,88 @@ namespace ConsoleAppSimple
             Environment.Exit(0);
         }
     }
-    public class NotFoundCommand : ICommand
+    public class UnrecognizedCommand : ICommand
     {
         public void Execute(List<string> arguments)
         {
-            Console.WriteLine($"Hello World! NotFoundCommand");
+            Console.WriteLine($"Command not recognized");
+        }
+    }
+    public class PrintDirectoryCommand : ICommand //pwd
+    {
+        public void Execute(List<string> arguments, FileSystemElement currentNode)
+        {
+            Console.WriteLine($"PrintDirectoryCommand");
+        }
+    }
+    public class CreateDirectoryCommand : ICommand //mkdir
+    {
+        public void Execute(List<string> arguments, FileSystemElement currentNode)
+        {
+            new FileSystemElement(arguments[0], currentNode, true);
+            Console.WriteLine($"Create Directory {arguments[0]}");
+        }
+    }
+    public class CreateFileCommand : ICommand //touch
+    {
+        public void Execute(List<string> arguments, FileSystemElement currentNode)
+        {
+            new FileSystemElement(arguments[0], currentNode, false);
+            Console.WriteLine($"Create Directory {arguments[0]}");
+        }
+    }
+
+    public class ListCommand : ICommand //ls
+    {
+        public void Execute(List<string> arguments, FileSystemElement currentNode)
+        {
+            Console.WriteLine($"ListCommand");
+        }
+    }
+
+    public class ChangeDirectoryCommand : ICommand  //cd
+    {
+        public void Execute(List<string> arguments, FileSystemElement currentNode)
+        {
+            Console.WriteLine($"ChangeDirectoryCommand");
         }
     }
     #endregion
 
     #region FILES REGION
 
+    //Directories and Files
+    public class FileSystemElement
+    {
+        public string Name { get; set; }
+        public FileSystemElement Parent { get; set; }
+        public bool IsDirectory { get; set; }
+        public List<FileSystemElement> Children { get; set; }
+
+        public FileSystemElement(string name, FileSystemElement parent, bool isDirectory = false)
+        {
+            Name = name;
+            Parent = parent;
+            IsDirectory = IsDirectory;
+        }
+
+        public string GetCurrentFullPath()
+        {
+            if (Parent == null)
+            {
+                return Name;
+            }
+            else
+            {
+                return Parent.GetCurrentFullPath() + "/" + Name;
+            }
+        }
+
+        //public void AddChildren(FileSystemElement node)
+        //{
+        //    Children.Add(node);
+        //}
+
+    }
     #endregion
 }
